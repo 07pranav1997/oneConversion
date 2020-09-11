b86752940935abd3d12ef5d3c6bad65a9f2efd3e
Pranav Ray <om.rai1997@gmail.com>
2020-09-11 17:49:52 +0530

    Bindings in angular added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'oneConversion';
+  title = 'My Project';
+
+  showMsg = true;
+  colorName = 'green';
+  switchVal = 0;
+
+  users = [
+    { userId: 1, firstname: 'john' },
+    { userId: 2, firstname: 'jackson' },
+    { userId: 3, firstname: 'jenny' },
+    { userId: 4, firstname: 'robin' },
+    { userId: 5, firstname: 'sherry' },
+    { userId: 6, firstname: 'jack' }
+  ];
+
+  clsName = 'one';
+  clsName2 = 'two';
+
+  colorVal = 'green';
+  placeHolderVal = 'Enter Value'
+
+  hrefVal = 'https://getcomics.info/';
+
+  updatedLink = 'https://comicbook.com/';
+
+  showAlert(name) {
+    alert("Hello " + name);
+  }
+
+  consoleLog() {
+    console.log('From the input focus event');
+  }
+
+  fname = "Pranav";
+
+  ShowFName(fname) {
+    alert(fname)
+  }
+
+  user = {
+    userId: 20,
+    name: 'Pranav Ray',
+    DOB: '09/07/1997',
+    stipend: 15000.66
+  };
+  
+  readMe() {
+    console.log('button clicked');
+  }
 }
